074312c9114c98ff2eb38de149a7c7c68c97f723
dlwnsdud1320 <>
2021-08-21 05:18:13 +0000

    updata0821

--- app.js
+++ app.js
@@ -3,13 +3,26 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();            ///stop///
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  greeting.innerText = `hello ${username}`;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+function paintGreetings(username){
+  greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-loginForm.addEventListener("submit", onLoginSubmit);
+if(savedUsername === null){
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+}else{
+  paintGreetings(savedUsername);
+}
+
+///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
