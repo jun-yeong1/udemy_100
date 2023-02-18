ffb7f56a297e27c945e7320b303aa8ec88614f21
jun-yeong1 <dlwnsdud1320@gmail.com>
2023-02-18 19:45:08 +0900

    playername change

--- js/config.js
+++ js/config.js
@@ -7,3 +7,12 @@ function closePlayerConfig() {
   playerConfigOverlayElement.style.display = 'none';
   backdropElement.style.display = 'none';
 }
+
+function savePlayerConfig(event) {
+  event.preventDefault(); // 페이지 로드되지 않음
+  const formData = new FormData(event.target); // 객체 인스턴스화
+  const enteredPlayername = formData.get('playername').trim(); // 입력 데이터, trim() 공백제거
+  if (!enteredPlayername) { // playername이 공백인지 확인
+    alert
+  }
+}
